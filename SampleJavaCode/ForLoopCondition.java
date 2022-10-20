343932047c3dbf056632f88d40da1ffdaa56e490
Kamalakannan Anandan <kamalakannan.anandan@gmail.com>
2022-10-20 11:35:46 -0500

    Initial Commit

---
+++ SampleJavaCode/ForLoopCondition.java
@@ -0,0 +1,14 @@
+package dailyprogram;
+
+public class ForLoopCondition {
+
+	public static void main(String[] args) {
+		for ( int nValue = 0; nValue < 100; nValue++)
+		{
+			System.out.println("For Loop Condition Values are:" + nValue);
+			
+		}
+
+	}
+
+}
---
+++ SampleJavaCode/HelloWorld.java
@@ -0,0 +1,10 @@
+package dailyprogram;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+
+	}
+
+}
